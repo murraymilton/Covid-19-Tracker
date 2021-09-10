5323d7b822204a75f442d2e97e8c99002bdacc72
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-09-10 19:30:35 +0200

    Added the api call for gathering data for the chart

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Cards, Chart, CountryPicker} from './components'
+import styles from './App.module.css'
+import {fetchData} from './api';
+
+function App() {
+    return (
+        <div className={styles.container}>
+           <Cards/>
+           <CountryPicker/>
+           <Chart/>
+        </div>
+    )
+}
+
+export default App
