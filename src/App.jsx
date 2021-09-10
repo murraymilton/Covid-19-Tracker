224a0b970d771da045e29339145150b0e0cb7062
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-09-10 23:12:23 +0200

    Added the country selection for filtering the selection of individual countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
-import {Cards, Chart, CountryPicker} from './components'
-import styles from './App.module.css'
-import {fetchData} from './api';
+
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
 
 class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
 
-    async componentDidMount() {
-        const data = await fetchData();
-        console.log(data)
-    }
-    render(){
-        return(
-            <div className={styles.container}>
-                <Cards />
-                <CountryPicker />
-                <Chart />
-            </div>
-        )
-    }
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
